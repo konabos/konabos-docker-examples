fa5be572dbf9c0250becd7dbf02806fab35b7057
Kamruz Jaman <kj@konabos.com>
2021-02-02 20:10:10 -0500

    Removed unused commented code

--- sitecore-xm1/docker/clean.ps1
+++ sitecore-xm1/docker/clean.ps1
@@ -3,10 +3,6 @@ param(
   [switch]$IncludeDatabases
 )
 
-# stop and remove all containers
-#docker ps -a | % { docker stop $_ }
-#docker ps -a | % { docker rm $_ }
-
 # Remove all unsused conrainers, networks, images, volumes
 docker system prune -f
 
